711371ee03b180ba4692e05d2b1adf7b2fce9c0a
iseungho <ghehf51@naver.com>
2022-09-25 01:11:39 +0900

    test

--- JAVA/1. 입출력과 사칙연산/BOJ_2557_Hello_World.java
+++ JAVA/1. 입출력과 사칙연산/BOJ_2557_Hello_World.java
@@ -4,6 +4,6 @@
 public class BOJ_2557_Hello_World {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!");
     }
 }
